5434311c97fca0bb9570b33199596ab94f380f6a
sulekaustubh <sulekaustubh97@gmail.com>
2023-09-14 22:35:30 +0530

    updated

---
+++ Arrays/test.js
@@ -0,0 +1,29 @@
+// let arr = [
+// 	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+// ];
+
+// console.log(Math.max(...arr))
+
+const x = 20;
+y = 1;
+console.log(y);
+
+// let count = [];
+// for (i = 0; i < arr.length; i++){
+//     if (arr[i] % 2 !== 0 && arr[i] % 3 !== 0) {
+
+//         count.push(arr[i]);
+//     }
+// }
+// console.log(count);
+// let a;
+// let b;
+// let c;
+
+// a = 3;
+// b = ++a;
+// c = a++;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
