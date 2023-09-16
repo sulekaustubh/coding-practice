132d46cc1811d25419d01abc09559d2b70f22abe
sulekaustubh <sulekaustubh97@gmail.com>
2023-09-16 23:26:13 +0530

    Update test.js

--- Arrays/test.js
+++ Arrays/test.js
@@ -1,12 +1,28 @@
-// let arr = [
-// 	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let temp1 = Math.max(...arr)
+console.log(temp1)
+let x = arr.indexOf(temp1)
+arr[x] = 0;
+let temp2 = Math.max(...arr)
+console.log(temp2)
+
+let sum = temp1 + temp2
+console.log(`total is ${sum} `)
+
+console.log(arr)
+
+
+
+
+//     , 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 // ];
 
 // console.log(Math.max(...arr))
 
-const x = 20;
-y = 1;
-console.log(y);
+// const x = 20;
+// y = 1;
+// console.log(y);
 
 // let count = [];
 // for (i = 0; i < arr.length; i++){
