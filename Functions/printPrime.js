e49a9861cb010fca76544c7400e5ddfeaaf9f94b
sulekaustubh <sulekaustubh97@gmail.com>
2023-09-21 22:25:02 +0530

    added printPrime.js

---
+++ Functions/printPrime.js
@@ -0,0 +1,27 @@
+// write a function to print all the PRIME numbers between 1 and n
+
+function printPrime(m) {
+    let arr = [];
+
+    for (j = 1; j <= m; j++){
+        if (isPrime(j)) {
+            arr.push(j);
+        }
+    }
+    return console.log(arr);
+}
+
+function isPrime(n) {
+	let count = 0;
+
+	for (i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			count++;
+		}
+	}
+	if (count == 2) {
+		return true;
+	}
+}
+
+printPrime(20);
