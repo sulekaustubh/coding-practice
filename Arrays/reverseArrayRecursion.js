4e05beea611040631bfccbdd065af7d8aec80f81
sulekaustubh <sulekaustubh97@gmail.com>
2023-09-25 16:01:47 +0530

    Update reverseArrayRecursion.js

--- Arrays/reverseArrayRecursion.js
+++ Arrays/reverseArrayRecursion.js
@@ -1,7 +1,7 @@
 // EASY
 // reverse an array using recursion
 // do not create a new array
-// do not use in-built methods like reverse
+// do not use in-built methods like .reverse()
 
 function reverseArrayRecursion(arr) {
     
@@ -24,3 +24,5 @@ function reverseArrayRecursion(arr) {
 }
 
 reverseArrayRecursion([6, 5, 4, 3, 2, 1]);
+
+// to get better complexity, use WHILE loop instead of recursion
