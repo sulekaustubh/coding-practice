c1256cd1e66601aa381de9c2277a776b174cd7f1
sulekaustubh <sulekaustubh97@gmail.com>
2023-09-27 04:53:39 +0530

    Create isPalindrome.js

---
+++ Functions/isPalindrome.js
@@ -0,0 +1,9 @@
+// VERY EASY
+// write a function to check whether a given string is a Palindrome or not
+
+function isPalindrome(str) {
+    let revStr = str.split('').reverse().join('')
+    return revStr == str ? 'true' : 'false';
+}
+
+console.log(isPalindrome('wowow'));
