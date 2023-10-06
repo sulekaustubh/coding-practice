5f93812aa9945a6200a05e22e2aa32cdb3474a7f
sulekaustubh <sulekaustubh97@gmail.com>
2023-10-06 16:13:21 +0530

    added more questions

--- Arrays/test.js
+++ Arrays/test.js
@@ -1,41 +1,48 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let temp1 = Math.max(...arr)
-console.log(temp1)
-let x = arr.indexOf(temp1)
-arr[x] = 0;
-let temp2 = Math.max(...arr)
-console.log(temp2)
+// let temp1 = Math.max(...arr)
+// console.log(temp1)
+// let x = arr.indexOf(temp1)
+// arr[x] = 0;
+// let temp2 = Math.max(...arr)
+// console.log(temp2)
 
-let sum = temp1 + temp2
-console.log(`total is ${sum} `)
+// let sum = temp1 + temp2
+// console.log(`total is ${sum} `)
 
+// //   10  , 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+// // ];
 
-//   10  , 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-// ];
+// // console.log(Math.max(...arr))
 
-// console.log(Math.max(...arr))
+// // const x = 20;
+// // y = 1;
+// // console.log(y);
 
-// const x = 20;
-// y = 1;
-// console.log(y);
+// // let count = [];
+// // for (i = 0; i < arr.length; i++){
+// //     if (arr[i] % 2 !== 0 && arr[i] % 3 !== 0) {
 
-// let count = [];
-// for (i = 0; i < arr.length; i++){
-//     if (arr[i] % 2 !== 0 && arr[i] % 3 !== 0) {
+// //         count.push(arr[i]);
+// //     }
+// // }
+// // console.log(count);
+// // let a;
+// // let b;
+// // let c;
 
-//         count.push(arr[i]);
-//     }
+// // a = 3;
+// // b = ++a;
+// // c = a++;
+
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+// for (let i = 1; i <= 5; i++){
+//     setTimeout(function () {
+//         console.log('Called' + i);
+//     }, 1000 * i);
 // }
-// console.log(count);
-// let a;
-// let b;
-// let c;
 
-// a = 3;
-// b = ++a;
-// c = a++;
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
